15e7fa98fa72534619382dc64db34448513f8a5a
vskarleas <vskarleas@icloud.com>
2021-03-28 23:41:54 +0300

    Body has been inserted

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(
@@ -18,13 +19,14 @@ void main() {
                     TextSpan(
                       text: '\nCalendar View',
                       style: TextStyle(
-                        color: Color.fromRGBO(120, 120, 120, 0.9),
+                        color: Color.fromRGBO(120, 120, 120, 1),
                         fontSize: 18,
                       ),
                     ),
                   ]),
             ),
             backgroundColor: Color.fromRGBO(244, 244, 244, 1)),
+        body: Text('This is the body text'),
       ),
     ),
   );
